3930c21ec80dc89f9935afa66360732e27420550
Franjo Pongrac <franjo.pongrac@adriaticnetworks.com>
2026-04-02 18:35:06 +0000

    end of ch2

---
+++ movies/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path
+from movies.api import MovieListCreateAPIView, MovieDetailAPIView
+
+app_name = 'movies' # Define the application namespace
+
+urlpatterns = [
+    path('movies/', MovieListCreateAPIView.as_view(), name='movie-list'),
+    path('movies/<int:pk>/', MovieDetailAPIView.as_view(), name='movie-detail'),
+]
